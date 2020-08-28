67c2f5ba72dd2f70e959f4a9805a0e9d186542c4
samimeadad <sami.meadad@gmail.com>
2020-08-28 11:57:43 +0600

    practice continued

---
+++ prog_3_10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char ch = 'D';
+
+    if (ch>='a' && ch<='z')
+        printf("%c is lower case\n", ch);
+
+    else if (ch>='A' && ch<'z')
+        printf("%c is upper case\n", ch);
+
+    else
+        printf("%c is not a letter", ch);
+
+    return 0;
+}
--- prog_3_9.c
+++ prog_3_9.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    int n=11;
-    if(n%2 == 0)
+    int n=5;
+    if(n%2==0)
         printf("The number is even.\n");
     else
         printf("The number is odd.\n");
