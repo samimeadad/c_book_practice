7a720bbe5279c4dbea519ce89bda6a980c911cda
samimeadad <sami.meadad@gmail.com>
2020-08-28 11:45:33 +0600

    CH_3_Exc_1

--- Exercise_3_1.c
+++ Exercise_3_1.c
@@ -2,9 +2,14 @@
 
 int main(void)
 {
-    int n=5;
-    int rem = n%2;
-    if(rem==0)
+    int n=3;
+    int rem = 0;
+
+    while(n >= 2)
+        n = n-2;
+    rem = n;
+
+    if(rem == 0)
         printf("The number is even.\n");
     else
         printf("The number is odd.\n");
--- prog_3_9.c
+++ prog_3_9.c
@@ -2,9 +2,8 @@
 
 int main(void)
 {
-    int n=5;
-    int rem = n%2;
-    if(rem==0)
+    int n=11;
+    if(n%2 == 0)
         printf("The number is even.\n");
     else
         printf("The number is odd.\n");
