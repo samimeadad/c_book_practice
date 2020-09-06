b7e4d0714b92de96ad7f5ada1a3b1551d7ca3a37
samimeadad <sami.meadad@gmail.com>
2020-09-06 20:00:21 +0600

    Book Exercise

---
+++ Exercise_4_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+    int i, j, m;
+
+    for(i=1; i<=20; i++)
+    {
+        m = 0;
+        printf("\n");
+        for(j=1; j<=10; j++)
+        {
+            m = m + i;
+            printf("%d * %d = %d\n", i, j, m);
+        }
+    }
+
+    return 0;
+}
---
+++ Exercise_5_1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(void)
+{
+    double loan_amount, int_rate, total_amount, monthly_installment;
+    int year, month, i;
+
+    printf("Loan Amount = ");
+    scanf("%lf", &loan_amount);
+
+    printf("Interest Rate = ");
+    scanf("%lf", &int_rate);
+
+    printf("Year = ");
+    scanf("%d", &year);
+
+    total_amount = loan_amount;
+
+    for(i=0; i<year; i++)
+        total_amount = total_amount + (total_amount*int_rate)/100;
+
+    monthly_installment = total_amount/(year*12);
+
+    printf("Total Amount for %d year = %.2lf\n", year, total_amount);
+    printf("Monthly Installment = %.2lf\n", monthly_installment);
+
+    return 0;
+}
