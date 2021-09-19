85ec419288f5a0816986258e028b79f9df876076
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-09-19 23:55:24 +0600

    done

--- Exercise_5_1.c
+++ Exercise_5_1.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -16,10 +16,10 @@ int main(void)
 
     total_amount = loan_amount;
 
-    for(i=0; i<year; i++)
-        total_amount = total_amount + (total_amount*int_rate)/100;
+    for (i = 0; i < year; i++)
+        total_amount = total_amount + (total_amount * int_rate) / 100;
 
-    monthly_installment = total_amount/(year*12);
+    monthly_installment = total_amount / (year * 12);
 
     printf("Total Amount for %d year = %.2lf\n", year, total_amount);
     printf("Monthly Installment = %.2lf\n", monthly_installment);
